92d2d3b534ef9695b5032a9b25ec3c086d6644bd
Mark Wang <Mark.Wang@quest.com>
2020-06-12 11:49:48 +0800

    new methods added

--- GitMethods/Program.cs
+++ GitMethods/Program.cs
@@ -8,8 +8,46 @@ namespace GitMethods
 {
     class Program
     {
+        int i, j;
+
+        private void AccpetDetails()
+        {
+            //input numbers
+            Console.WriteLine("Please enter 2 number:");
+            i = Convert.ToInt32(Console.ReadLine());
+            j = Convert.ToInt32(Console.ReadLine());
+        }
+
+        private void MaxNumber()
+        {
+            if (i > j)
+            {
+                Console.WriteLine("Max number is {0}", i);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Max number is {0}", j);
+                Console.ReadLine();
+            }
+        }
+
+        private void MinNumber()
+        {
+            if (i < j)
+            {
+                Console.WriteLine("Max number is {0}", i);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Max number is {0}", j);
+                Console.ReadLine();
+            }
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
